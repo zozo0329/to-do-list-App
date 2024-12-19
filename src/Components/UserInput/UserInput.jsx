049e5da18713d492919e5982a6a603a8b9590494
Zo <161946731+zozo0329@users.noreply.github.com>
2024-12-19 19:01:35 +0800

    mUpdate

--- src/Components/UserInput/UserInput.jsx
+++ src/Components/UserInput/UserInput.jsx
@@ -1,11 +1,59 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import userStyle from "./UserInput.module.css";
+
+const Backdrop = (props) => {
+  return <div className={userStyle.backdrop} onClick={props.onClick} />;
+};
+const Modal = (props) => {
+  return (
+    <div className={userStyle.modal}>
+      <div className={userStyle.modalContent}>
+        <div className={userStyle.modalHeader}>
+          <h2
+            className={`${userStyle.label} ${
+              !props.isValid && userStyle.invalid
+            }`}
+          >
+            Enter List
+          </h2>
+        </div>
+        <form onSubmit={props.onSubmit}>
+          <div className={userStyle.modalBody}>
+            <input
+              className={`${userStyle.input} ${
+                !props.isValid && userStyle.invalidInput
+              }`}
+              type="text"
+              onChange={props.onChange}
+              value={props.value}
+            />
+          </div>
+          <div className={userStyle.modalFooter}>
+            <div className={userStyle.button}>
+              <button
+                className={userStyle.buttonStyle}
+                type="button"
+                onClick={props.onClick}
+              >
+                Cancel
+              </button>
+              <button className={userStyle.buttonStyle} type="submit">
+                Confirm
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 const UserInput = (props) => {
   const [userInput, setUserInput] = useState("");
   const [isValid, setIsValid] = useState(true);
   const changeHandler = (e) => {
     setUserInput(e.target.value);
-    if (userInput.trim().length >= 1) {
+    if (userInput.trim().length > 0) {
       setIsValid(true);
     }
   };
@@ -28,46 +76,20 @@ const UserInput = (props) => {
   };
   return (
     <>
-      <div className={userStyle.backdrop} onClick={cancelHandler} />
-      <div className={userStyle.modal}>
-        <div className={userStyle.modalContent}>
-          <div className={userStyle.modalHeader}>
-            <h2
-              className={`${userStyle["label"]} ${
-                !isValid && userStyle.invalid
-              }`}
-            >
-              Enter List
-            </h2>
-          </div>
-          <form onSubmit={submitHandler}>
-            <div className={userStyle.modalBody}>
-              <input
-                className={`${userStyle["input"]} ${
-                  !isValid && userStyle.invalidInput
-                }`}
-                type="text"
-                onChange={changeHandler}
-                value={userInput}
-              />
-            </div>
-            <div className={userStyle.modalFooter}>
-              <div className={userStyle.button}>
-                <button
-                  className={userStyle.buttonStyle}
-                  type="button"
-                  onClick={cancelHandler}
-                >
-                  Cancel
-                </button>
-                <button className={userStyle.buttonStyle} type="submit">
-                  Confirm
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={cancelHandler} />,
+        document.querySelector("#backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          onSubmit={submitHandler}
+          onClick={cancelHandler}
+          onChange={changeHandler}
+          value={userInput}
+          isValid={isValid}
+        />,
+        document.querySelector("#modal-root")
+      )}
     </>
   );
 };
