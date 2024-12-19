2796de2ff234524ac5deb0a763d33fcbd68d46c9
Zo <161946731+zozo0329@users.noreply.github.com>
2024-12-19 11:26:52 +0800

    minor update

--- src/Components/UserInput/UserInput.jsx
+++ src/Components/UserInput/UserInput.jsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import userStyle from "./UserInput.module.css";
 const UserInput = (props) => {
   const [userInput, setUserInput] = useState("");
+  const [isValid, setIsValid] = useState(true);
   const changeHandler = (e) => {
     setUserInput(e.target.value);
+    if (userInput.trim().length >= 1) {
+      setIsValid(true);
+    }
   };
   const cancelHandler = () => {
     props.setIsOn(false);
@@ -11,12 +15,15 @@ const UserInput = (props) => {
   };
   const submitHandler = (e) => {
     e.preventDefault();
+    if (userInput.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     const userData = {
       userInput,
       id: Math.random().toString(),
     };
     props.addInputHandler(userData);
-    // CLEANER
     setUserInput("");
   };
   return (
@@ -25,12 +32,20 @@ const UserInput = (props) => {
       <div className={userStyle.modal}>
         <div className={userStyle.modalContent}>
           <div className={userStyle.modalHeader}>
-            <h2>Enter List</h2>
+            <h2
+              className={`${userStyle["label"]} ${
+                !isValid && userStyle.invalid
+              }`}
+            >
+              Enter List
+            </h2>
           </div>
           <form onSubmit={submitHandler}>
             <div className={userStyle.modalBody}>
               <input
-                className={userStyle.input}
+                className={`${userStyle["input"]} ${
+                  !isValid && userStyle.invalidInput
+                }`}
                 type="text"
                 onChange={changeHandler}
                 value={userInput}
